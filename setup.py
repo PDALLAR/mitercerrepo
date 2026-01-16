bd5258f99aff9a584e5a861f04288c6a959937b6
Pablo Dallar <pdallar@gmail.com>
2026-01-16 09:56:50 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercer Repo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Pedro Dallar",                         # Tu nombre
+    author_email="pdallar@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/platziPDALLAR/mitercerrepo",     # URL del proyecto
+)
